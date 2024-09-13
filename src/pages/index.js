c2ee78049810449371e5d53172ead7b88cfc1ae6
Samuel Krohn <sanikr@outlook.com>
2024-09-13 14:25:23 -0600

    Create fetch request

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 import * as React from "react"
 
+
+
 const IndexPage = () => {
+ const sendEmail = (e) => {
+  e.preventDefault()
+  
+  fetch('https://kkwvehdtf6absgcda7uvbqi3vq0tpnhv.lambda-url.us-east-1.on.aws/', {
+    method: 'POST',
+    // headers: {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json',
+    // },
+    // body: JSON.stringify({
+    //   firstParam: 'yourValue',
+    //   secondParam: 'yourOtherValue',
+    // })
+  })
+  .catch(err => console.log(err))
+  .then(res => console.log(res))
+ }
+
   return (
 
-<form action="https://kkwvehdtf6absgcda7uvbqi3vq0tpnhv.lambda-url.us-east-1.on.aws/" method="post">
-  <button type="button">Submit</button>
-
+<form>
+  <button type="button" onClick={sendEmail}>Submit</button>
 </form>
   )
 }
