3b39617a7dac09e427da0e050c60880fc07093f1
Samuel Krohn <sanikr@outlook.com>
2025-03-01 15:54:49 -0700

    Add SEO import

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout/Layout";
+import SEO from "../components/SEO/SEO";
 import * as styles from "./about.module.scss";
 
 const About = () => {
