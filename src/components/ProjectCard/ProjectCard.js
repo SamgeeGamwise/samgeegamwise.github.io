588571f08f7c4833b6c46d87cdfc4538e8fe9977
Samuel Krohn <sanikr@outlook.com>
2025-12-17 09:20:13 -0700

    Fix projects

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,20 +2,24 @@ import React from 'react'
 import * as styles from './ProjectCard.module.scss'
 
 const ProjectCard = ({ project, index }) => {
+  const technologies = project.technologies ?? []
+
   return (
     <div key={index} className={styles.projectCard}>
       <h2 className={styles.projectName}>{project.name}</h2>
       <p className={styles.projectDescription}>{project.description}</p>
-      <p>{project.technologies.map((technology, key) => 
-        key === 0 && project.technologies.length > 1 ? 
-          <><a key={key} href={technology.url} target="_blank" rel="noopener noreferrer" className={styles.projectTechnologies}>
-            {technology.name}
-          </a><span>, </span></> : 
-          <a key={key} href={technology.url} target="_blank" rel="noopener noreferrer" className={styles.projectTechnologies}>
-          {technology.name}
-        </a>
-        )}
-      </p>
+      {technologies.length > 0 && (
+        <p>
+          {technologies.map((technology, key) => (
+            <React.Fragment key={key}>
+              {key > 0 && <span>, </span>}
+              <a href={technology.url} target="_blank" rel="noopener noreferrer" className={styles.projectTechnologies}>
+                {technology.name}
+              </a>
+            </React.Fragment>
+          ))}
+        </p>
+      )}
       <div className={styles.projectLinks}>
         {project.liveLink && (
           <a href={project.liveLink} target="_blank" rel="noopener noreferrer" className={styles.liveLink}>
