f42c12314bc0feff7b7346e587aa2c60c8574c92
Samuel Krohn <sanikr@outlook.com>
2025-12-17 09:09:24 -0700

    Update Projects list

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,54 +1,57 @@
-import React from "react";
-import Layout from "../components/Layout/Layout";
-import ProjectCard from "../components/ProjectCard/ProjectCard";
-import BackToHome from "../components/BackToHome/BackToHome";
-import { headData } from "../head";
-import * as styles from "./projects.module.scss";
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import ProjectCard from '../components/ProjectCard/ProjectCard'
+import BackToHome from '../components/BackToHome/BackToHome'
+import { headData } from '../head'
+import * as styles from './projects.module.scss'
 
 const projects = [
   {
-    name: "SolarCleanColorado",
-    description: "A website for a solar panel cleaning service.",
-    technologies: [{name: "Gatsby.js", url: "https://github.com/Solarcleancolorado/solarcleancolorado.github.io"}],
-    liveLink: "https://www.solarcleancolorado.com/"
+    name: 'DK Roofing, Painting, & Gutters',
+    description: 'Local roofing and painting business website.',
+    technologies: [{ name: 'Eleventy + Nunjucks', url: 'https://github.com/SamgeeGamwise/DarleneKay' }],
+    liveLink: 'https://darlenekay.com/',
   },
   {
-    name: "30 Minute Pomodoro Timer",
-    description: "A desktop application implementing the Pomodoro technique.",
-    technologies: [{name: "Electron", url: "https://github.com/SamgeeGamwise/Pomodoro"}],
+    name: 'Solar Clean Colorado',
+    description: 'Local solar cleaning business website.',
+    technologies: [{ name: 'Gatsby.js', url: 'https://github.com/Solarcleancolorado/solarcleancolorado.github.io' }],
+    liveLink: 'https://samgee.gamwise.me/solarcleancolorado/',
   },
   {
-    name: "Dinnerio",
-    description: "A meal planning application.",
-    technologies: [{name: "Angular", url: "https://github.com/SamgeeGamwise/dinnerio-angular"}, {name: "NestJS", url: "https://github.com/SamgeeGamwise/dinnerio"}],
+    name: '30 Minute Pomodoro Timer',
+    description: 'A desktop application implementing the Pomodoro technique.',
+    technologies: [{ name: 'Electron', url: 'https://github.com/SamgeeGamwise/Pomodoro' }],
   },
   {
-    name: "HarperDB NPM Package",
-    description: "An NPM package for interacting with HarperDB.",
-    technologies: [{name: "Node", url: "https://github.com/SamgeeGamwise/harperdb-functions"}],
-    liveLink: "https://www.npmjs.com/package/harperdb-node"
+    name: 'HarperDB NPM Package',
+    description: 'An NPM package for interacting with HarperDB.',
+    liveLink: 'https://www.npmjs.com/package/harperdb-node',
   },
   {
-    name: "Announcement Bot",
-    description: "A Discord bot for managing announcements.",
-    technologies: [{name: "Node", url: "https://github.com/bubbzDotDev/bot-dashboard-backend"}],
+    name: 'Announcement Bot',
+    description: 'A Discord bot for managing announcements.',
+    technologies: [{ name: 'Node', url: 'https://github.com/bubbzDotDev/bot-dashboard-backend' }],
   },
   {
-    name: "Advent of Code 2024",
-    description: "Solutions for Advent of Code 2024 challenges.",
-    technologies: [{name: "Rust", url: "https://github.com/SamgeeGamwise/advent-of-code-2024"}],
+    name: 'Advent of Code 2024',
+    description: 'Solutions for Advent of Code 2024 challenges.',
+    technologies: [{ name: 'Rust', url: 'https://github.com/SamgeeGamwise/advent-of-code-2024' }],
   },
   {
-    name: "Dragons D&D",
-    description: "A Dungeons & Dragons companion app.",
-    technologies: [{name: "Vue", url: "https://github.com/SamgeeGamwise/dragons_laravel"}, {name: "Laravel", url: "https://github.com/SamgeeGamwise/dragons_backend"}],
+    name: 'Dragons D&D',
+    description: 'A Dungeons & Dragons companion app.',
+    technologies: [
+      { name: 'Vue', url: 'https://github.com/SamgeeGamwise/dragons_laravel' },
+      { name: 'Laravel', url: 'https://github.com/SamgeeGamwise/dragons_backend' },
+    ],
   },
   {
-    name: "Asteroids Arcade Game",
-    description: "A classic arcade game implemented in Python.",
-    technologies: [{name: "Python", url: "https://github.com/SamgeeGamwise/asteroids"}],
+    name: 'Asteroids Arcade Game',
+    description: 'A classic arcade game implemented in Python.',
+    technologies: [{ name: 'Python', url: 'https://github.com/SamgeeGamwise/asteroids' }],
   },
-];
+]
 
 const Projects = () => {
   return (
@@ -65,16 +68,15 @@ const Projects = () => {
         <BackToHome />
       </Layout>
     </>
-  );
-};
+  )
+}
 
-export default Projects;
-
+export default Projects
 
 export const Head = () => (
   <>
     <title>{`Projects | Samuel Krohn - Portfolio`}</title>
-    { headData }
+    {headData}
     <meta name="description" content="Projects completed by Samuel Krohn for various reasons" />
   </>
 )
